9c568cd471eaf547035397c45caaf7c5a915753c
kaR07L6 <u.rinu.mao0624@icloud.com>
2025-08-04 11:54:14 +0900

    新規登録

--- user.php
+++ user.php
@@ -1,31 +1,30 @@
 <?php
 $error = "";
 
-if ($_SERVER["REQUEST_METHOD"] === "post") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $name = trim($_POST["name"]);
     $password = trim($_POST["password"]);
 
     if ($name === "" || $password === "") {
         $error = "名前とパスワードを入力してください。";
     } else {
-        $register_data = date("Y-m-d");
+        $register_date = date("Y-m-d");
         $user_id = uniqid("user_");
 
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
         $file = fopen("users.csv", "a");
-        fputcsv($file, [$user_id, $name, $password, $register_date]);
+        fputcsv($file, [$user_id, $name, $hashed_password, $register_date]);
         fclose($file);
 
         header("Location: list.php");
         exit();
     }
 }
-
-
 ?>
 
-
 <form method="post">
-    <label>Name<input type="text" name="name"></label><br>
+    <label>Name<input type="text" name="name" value="<?= htmlspecialchars($name ?? '') ?>"></label><br>
     <label>Password<input type="password" name="password"></label><br>
     <button type="submit">新規登録</button>
 </form>
